539650d43d3f53676a8c1f6c88e4d4370b1edcc8
TGBharath <108675637+TGBharath@users.noreply.github.com>
2022-10-12 23:53:53 +0530

    code

--- calculator.py
+++ calculator.py
@@ -1,1 +1,15 @@
 # calculator application for 2 numbers with + - / *
+a = $value1
+b = $value2
+
+operation = $Choices
+
+match operation:
+        case add:
+            return "a+b"
+        case sub:
+            return "a-b"
+        case mul:
+            return "a*b"
+        case div:
+            return "a/b"
